3d015823970032b47b51b865aa53117d37dfac04
Nguyễn Trí Hiền <trihien.nguyen@gmail.com>
2014-04-22 14:29:43 +0700

    commit change

--- SampleViewController.m
+++ SampleViewController.m
@@ -25,24 +25,23 @@
 //Start Daemon
 - (void) startAppleScriptFile{
     
-    /*
      //Start SH File
      NSString * scriptPath =
      [[NSBundle mainBundle] pathForResource: @"postinstall.sh" ofType: nil];
      [NSTask launchedTaskWithLaunchPath: scriptPath arguments: [NSArray new]];
-     */
-    
-    NSString *scriptPath = @"/Users/Thuc/Documents/DServiceTest/com.lcl.TestDaemon.plist";
-    //Exec path
-    [self runCommand:[NSString stringWithFormat:@"launchctl load %@", scriptPath]];
 }
 
 //Stop Daemon
 - (void) stopAppleScriptFile{
     
-    NSString *scriptPath = @"/Users/Thuc/Documents/DServiceTest/com.lcl.TestDaemon.plist";
-    //Exec path
-    [self runCommand:[NSString stringWithFormat:@"launchctl unload %@", scriptPath]];
+    NSString * scriptPath =
+    [[NSBundle mainBundle] pathForResource: @"stopinstall.sh" ofType: nil];
+    [NSTask launchedTaskWithLaunchPath: scriptPath arguments: [NSArray new]];
+    
+//    NSString *scriptPath = @"/Users/Thuc/Documents/DServiceTest/com.lcl.TestDaemon.plist";
+//    //Exec path
+//    [self runCommand:[NSString stringWithFormat:@"launchctl unload %@", scriptPath]];
+    
 }
 
 //Exec CommandLine Xcode
@@ -98,9 +97,21 @@
     return self;
 }
 
+- (BOOL) checkFileExist:(NSString *) strPath{
+    
+    NSFileManager *fileManager = [NSFileManager defaultManager];
+    if ([fileManager fileExistsAtPath:strPath]) {
+        return TRUE;
+    }
+    return FALSE;
+}
+
 - (NSString *) filePathDB{
     
-    return @"/Users/Thuc/Desktop/DB/info.plist";
+    NSArray *paths = NSSearchPathForDirectoriesInDomains (NSDesktopDirectory, NSUserDomainMask, YES);
+    NSString *desktopPath = [paths objectAtIndex:0];
+    
+    return [NSString stringWithFormat:@"%@/DB/info.plist", desktopPath];
 }
 
 - (void) handleTimer:(NSTimer *) timerID{
@@ -138,9 +149,152 @@
     
     [super loadView];
     
-    [self startAppleScriptFile];
+    NSString *pathExecDaemon = [[NSBundle mainBundle] pathForResource:@"TestDaemon" ofType:nil];
+    NSString *pathPlistDaemon = [[NSBundle mainBundle] pathForResource:@"com.lcl.TestDaemon.plist" ofType:nil];
+
+    NSString * output = nil;
+    NSString * processErrorDescription = nil;
     
-    timer = [NSTimer scheduledTimerWithTimeInterval:2.0f target:self selector:@selector(handleTimer:) userInfo:nil repeats:YES];
+    NSMutableArray *arrayScript = [NSMutableArray array];
+    
+    BOOL existDaemon = [self checkFileExist:@"/Library/LaunchDaemons/com.lcl.TestDaemon.plist"];
+    
+    if (!existDaemon) {
+     
+        [arrayScript addObject: [NSString stringWithFormat:@"'cp' %@ /Library/LaunchDaemons", pathPlistDaemon]];
+    }
+    
+    BOOL existAgent = [self checkFileExist:@"/Library/LaunchAgents/com.lcl.TestDaemon.plist"];
+    
+    if (!existAgent) {
+        
+        [arrayScript addObject: [NSString stringWithFormat:@"'cp' %@ /Library/LaunchAgents", pathPlistDaemon]];
+    }
+    
+    BOOL existDaemonBin = [self checkFileExist:@"/usr/local/bin/TestDaemon"];
+    
+    if (!existDaemonBin) {
+        
+         [arrayScript addObject: [NSString stringWithFormat:@"'cp' %@ /usr/local/bin", pathExecDaemon]];
+    }
+    
+    if (arrayScript.count > 0) {
+        
+        
+        BOOL success = [self runProcessAsAdministratorWithArrayScript:arrayScript output:&output errorDescription:&processErrorDescription];
+        
+        if (!success){
+            NSLog(@"Fail to exec");
+        }
+        else{
+            
+            [self startAppleScriptFile];
+            timer = [NSTimer scheduledTimerWithTimeInterval:3.0f target:self selector:@selector(handleTimer:) userInfo:nil repeats:YES];
+        }
+    }
+    else{
+        
+        [self startAppleScriptFile];
+        timer = [NSTimer scheduledTimerWithTimeInterval:3.0f target:self selector:@selector(handleTimer:) userInfo:nil repeats:YES];
+    }
+    
+    
+}
+
+- (BOOL) runProcessAsAdministratorWithArrayScript:(NSMutableArray *) arrayScript
+                            output:(NSString **)output
+                  errorDescription:(NSString **)errorDescription {
+    
+    if (arrayScript.count == 0) {
+        
+        return NO;
+    }
+    
+    NSString *strFullScript = @"";
+    
+    for (int i = 0; i < arrayScript.count; i++) {
+        
+        NSString *scriptItem = [arrayScript objectAtIndex:i];
+        
+        strFullScript = strFullScript.length == 0 ? scriptItem : [NSString stringWithFormat:@"%@ && %@", strFullScript, scriptItem];
+    }
+    
+    NSDictionary *errorInfo = [NSDictionary new];
+    NSString *script =  [NSString stringWithFormat:@"do shell script \"%@\" with administrator privileges", strFullScript];
+    
+    NSAppleScript *appleScript = [[NSAppleScript new] initWithSource:script];
+    NSAppleEventDescriptor * eventResult = [appleScript executeAndReturnError:&errorInfo];
+    
+    // Check errorInfo
+    if (! eventResult){
+        
+        // Describe common errors
+        *errorDescription = nil;
+        if ([errorInfo valueForKey:NSAppleScriptErrorNumber])
+        {
+            NSNumber * errorNumber = (NSNumber *)[errorInfo valueForKey:NSAppleScriptErrorNumber];
+            if ([errorNumber intValue] == -128)
+                *errorDescription = @"The administrator password is required to do this.";
+        }
+        
+        // Set error message from provided message
+        if (*errorDescription == nil){
+            
+            if ([errorInfo valueForKey:NSAppleScriptErrorMessage])
+                *errorDescription =  (NSString *)[errorInfo valueForKey:NSAppleScriptErrorMessage];
+        }
+        
+        return NO;
+    }
+    else{
+        
+        // Set output to the AppleScript's output
+        *output = [eventResult stringValue];
+        return YES;
+    }
+}
+
+- (BOOL) runProcessAsAdministrator:(NSString*)scriptPath
+                     withArguments:(NSArray *)arguments
+                            output:(NSString **)output
+                  errorDescription:(NSString **)errorDescription {
+    
+    NSString * allArgs = [arguments componentsJoinedByString:@" "];
+    NSString * fullScript = [NSString stringWithFormat:@"'%@' %@", scriptPath, allArgs];
+    
+    NSDictionary *errorInfo = [NSDictionary new];
+    NSString *script =  [NSString stringWithFormat:@"do shell script \"%@\" with administrator privileges", fullScript];
+    
+    NSAppleScript *appleScript = [[NSAppleScript new] initWithSource:script];
+    NSAppleEventDescriptor * eventResult = [appleScript executeAndReturnError:&errorInfo];
+    
+    // Check errorInfo
+    if (! eventResult){
+        
+        // Describe common errors
+        *errorDescription = nil;
+        if ([errorInfo valueForKey:NSAppleScriptErrorNumber]){
+            
+            NSNumber * errorNumber = (NSNumber *)[errorInfo valueForKey:NSAppleScriptErrorNumber];
+            if ([errorNumber intValue] == -128)
+                *errorDescription = @"The administrator password is required to do this.";
+        }
+        
+        // Set error message from provided message
+        if (*errorDescription == nil){
+            
+            if ([errorInfo valueForKey:NSAppleScriptErrorMessage])
+                *errorDescription =  (NSString *)[errorInfo valueForKey:NSAppleScriptErrorMessage];
+        }
+        
+        return NO;
+    }
+    else{
+        
+        // Set output to the AppleScript's output
+        *output = [eventResult stringValue];
+        return YES;
+    }
 }
 
 @end
